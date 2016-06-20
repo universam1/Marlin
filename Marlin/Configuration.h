bb767d1afb7414b46868bdac111a61b1fbe009c5
Lang, Samuel <slang@deu.wtbts.net>
2016-06-20 13:43:51 +0200

    SD enabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -812,7 +812,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 // SD Card support is disabled by default. If your controller has an SD slot,
 // you must uncomment the following option or it won't work.
 //
-//#define SDSUPPORT
+#define SDSUPPORT
 
 //
 // SD CARD: SPI SPEED
